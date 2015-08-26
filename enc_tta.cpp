bc5fe9d648c1b1333422ad45c6c9bcae34812f3b
Yamagata Fumihiro <bun@kushiro-ct.ac.jp>
2015-08-27 01:01:43 +0900

    Fix version name.

--- enc_tta.cpp
+++ enc_tta.cpp
@@ -101,7 +101,7 @@ extern "C"
 		if (idx == 0)
 		{
 			GetLocalisationApiService();
-			StringCchPrintfA(desc, 1024, WASABI_API_LNGSTRING(IDS_ENC_TTA_DESC), VERSION, LIBTTA_VERSION);
+			StringCchPrintfA(desc, 1024, WASABI_API_LNGSTRING(IDS_ENC_TTA_DESC), VERSION);
 			return mmioFOURCC('T', 'T', 'A', ' ');
 		}
 		return 0;
